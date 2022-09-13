2913b407287a34baed16d6b1e5ab3a0b6d109721
Solagne Lake <iriahbee@users.noreply.github.com>
2022-09-13 04:09:48 +0100

    sort added

--- src/main/java/lockedMe/com/SortFiles.java
+++ src/main/java/lockedMe/com/SortFiles.java
@@ -2,26 +2,27 @@ package lockedMe.com;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SortFiles {
 	
 	 
-	static List<String> fileList = new ArrayList<String>();
+	static List<String> fileList = new ArrayList<>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		File file = new File("C:\\Users\\user\\git\\projectphaseonemw\\src\\ExampleFiles");
         listDirectory(file);
+        Collections.sort(fileList);
         System.out.println(fileList);
         
 	}
         
         public static void listDirectory(File file) {
             if(file.isDirectory()) {
-                File[] files = file
-                		.listFiles();
+                File[] files = file.listFiles();
                 for(File currFile : files) {
                     listDirectory(currFile);
                     
@@ -29,6 +30,7 @@ public class SortFiles {
             }
             else {
                 fileList.add(file.getName());
+              
             }
        
           
